3cd76362192e8373fbd991ada411a9e2ad1eaa54
Mahdy Noureddine <mahdy.a.noureddine@gmail.com>
2022-01-09 12:47:31 -0500

    commit tree f1a9e37ac5623ddcfcd3d0b1c84ccfd5a3c903a3

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,82 @@
-import React from "react";
+import React , {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
+
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [newPlant, setNewPlant] = useState({
+                                              name: "",
+                                              image: "",
+                                              price: 0
+                                                        })
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then((response) => response.json())
+    .then((plantData) => setPlants(plantData))
+  },[])
+
+  function handleNewPlant(event) {
+    if(event.target.name === "price") {
+      const num = parseFloat(event.target.value)
+
+      setNewPlant({
+        ...newPlant,
+        [event.target.name] : num
+      })
+    }
+    else {
+      setNewPlant({
+        ...newPlant,
+        [event.target.name] : event.target.value
+      })
+    }
+  }
+
+  function handleNewPlantSubmit(event) {
+    event.preventDefault()
+
+    fetch('http://localhost:3000/plants', {
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify(newPlant)
+    })
+    .then((response) => response.json())
+    .then((newPlantItem) => {
+      setPlants([
+        ...plants,
+        newPlantItem
+      ])
+    })
+
+    setNewPlant({
+      name: "",
+      image: "",
+      price: 0
+                })
+  }
+
+  function handleSearch(event) {
+    setSearch(event.target.value)
+  }
+
+  const plantsToBeShown = plants.filter((plant) => {
+    if(search.length > 0) {
+      return plant.name.toLowerCase().includes(search.toLowerCase())
+    }
+    else {return true}
+  })
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm newPlant = {newPlant} handleNewPlant = {handleNewPlant} handleNewPlantSubmit={handleNewPlantSubmit} />
+      <Search search ={search} handleSearch = {handleSearch} />
+      <PlantList plants = {plantsToBeShown} />
     </main>
   );
 }
